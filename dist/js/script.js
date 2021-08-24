e0d4813e432ed89fdb658c644015d0e10a14874f
Pavel <pavelfedorov929@gmail.com>
2021-08-24 22:11:37 +0300

    переведены px в rem

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,13 @@ $(function () {
     $('.burger-btn').on('click', function () {
     $('.overlay').fadeIn('400');
     $('.burger-menu').addClass('burger-menu--active');
+    $('body').addClass('_lock');
 });
 
 $('.menu__link, .burger-menu__link, .burger-menu__btn, .burger-menu__close').on('click', function () {
     $('.overlay').fadeOut('400');
     $('.burger-menu').removeClass('burger-menu--active');
+    $('body').removeClass('_lock');
 });
 
     $('.open-popup').magnificPopup({
--- src/blocks/modules/burger/burger.js
+++ src/blocks/modules/burger/burger.js
@@ -1,9 +1,11 @@
 $('.burger-btn').on('click', function () {
     $('.overlay').fadeIn('400');
     $('.burger-menu').addClass('burger-menu--active');
+    $('body').addClass('_lock');
 });
 
 $('.menu__link, .burger-menu__link, .burger-menu__btn, .burger-menu__close').on('click', function () {
     $('.overlay').fadeOut('400');
     $('.burger-menu').removeClass('burger-menu--active');
+    $('body').removeClass('_lock');
 });
